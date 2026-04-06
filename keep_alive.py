9e80dd8e9f554cc8e2c634c88a12a6efd267ad66
mmerichukwuremie-maker <mmerichukwu.remie@ellenbrook.wa.edu.au>
2026-04-06 16:10:23 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
+import sqlite3
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    conn = sqlite3.connect('transactions.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM transactions ORDER BY id DESC LIMIT 50")
+    transactions = c.fetchall()
+
+    conn.close()
+
+    return render_template("dashboard.html", transactions=transactions)
 
 def run():
     app.run(host='0.0.0.0', port=8080)
